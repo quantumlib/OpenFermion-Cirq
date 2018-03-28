f1e74a339a82f778ad59ac49bb535d6d2e03decb
Kevin J. Sung <kevinsung@users.noreply.github.com>
2018-03-27 17:56:44 -0700

    Define LinearQubit and PlanarQubit (#7)
    * add LinearQubit
    
    * define PlanarQubit, remove ascii functions
    
    * rename (row, col) -> (x, y) and remove extraneous tests
    
    * use absolute imports, finish renaming variables, lint

---
+++ openfermioncirq/planar_qubit.py
@@ -0,0 +1,42 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from cirq.ops import QubitId
+
+
+class PlanarQubit(QubitId):
+    """A qubit on a 2-dimensional square lattice."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def is_adjacent(self, other: 'PlanarQubit') -> bool:
+        return abs(self.x - other.x) + abs(self.y - other.y) == 1
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __hash__(self):
+        return hash((PlanarQubit, self.x, self.y))
+
+    def __repr__(self):
+        return 'PlanarQubit({}, {})'.format(self.x, self.y)
+
+    def __str__(self):
+        return '({}, {})'.format(self.x, self.y)
---
+++ openfermioncirq/planar_qubit_test.py
@@ -0,0 +1,47 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from cirq.testing import EqualsTester
+
+from openfermioncirq.planar_qubit import PlanarQubit
+
+
+def test_planar_qubit_init():
+    q = PlanarQubit(3, 4)
+    assert q.x == 3
+    assert q.y == 4
+
+
+def test_planar_qubit_eq():
+    eq = EqualsTester()
+    eq.make_equality_pair(lambda: PlanarQubit(0, 0))
+    eq.make_equality_pair(lambda: PlanarQubit(1, 0))
+    eq.make_equality_pair(lambda: PlanarQubit(0, 1))
+    eq.make_equality_pair(lambda: PlanarQubit(50, 25))
+
+
+def test_planar_qubit_is_adjacent():
+    assert PlanarQubit(0, 0).is_adjacent(PlanarQubit(0, 1))
+    assert PlanarQubit(0, 0).is_adjacent(PlanarQubit(0, -1))
+    assert PlanarQubit(0, 0).is_adjacent(PlanarQubit(1, 0))
+    assert PlanarQubit(0, 0).is_adjacent(PlanarQubit(-1, 0))
+
+    assert not PlanarQubit(0, 0).is_adjacent(PlanarQubit(+1, -1))
+    assert not PlanarQubit(0, 0).is_adjacent(PlanarQubit(+1, +1))
+    assert not PlanarQubit(0, 0).is_adjacent(PlanarQubit(-1, -1))
+    assert not PlanarQubit(0, 0).is_adjacent(PlanarQubit(-1, +1))
+
+    assert not PlanarQubit(0, 0).is_adjacent(PlanarQubit(2, 0))
+
+    assert PlanarQubit(500, 999).is_adjacent(PlanarQubit(501, 999))
+    assert not PlanarQubit(500, 999).is_adjacent(PlanarQubit(5034, 999))
