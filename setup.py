4a3ba4a3edf0fb9b50c1d224077959b926836d9c
Ryan Babbush <ryanbabbush@gmail.com>
2018-06-27 11:11:13 -0700

    Configuring setup (#74)
    * updated setup script so it can run without Cirq being public
    
    * changed readme to rst
    
    * configured travis to still work
    
    * fixed pylint error
    
    * configured travis to use dev version of OpenFermion
    
    * use dev version of openfermion in travis

--- setup.py
+++ setup.py
@@ -10,11 +10,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import io
 from setuptools import find_packages, setup
 
 # This reads the __version__ variable from openfermioncirq/_version.py
 __version__ = None
 exec(open('openfermioncirq/_version.py').read())
+
+# Readme file as long_description:
+long_description = io.open('README.rst', encoding='utf-8').read()
 
 # Read in requirements.txt
 requirements = open('requirements.txt').readlines()
@@ -24,7 +28,7 @@ setup(
     name='OpenFermion-Cirq',
     version=__version__,
     url='https://github.com/quantumlib/OpenFermion-Cirq',
-    author='The OpenFermion-Cirq Developers',
+    author='The OpenFermion Developers',
     install_requires=requirements,
     license='Apache 2',
     packages=find_packages())
